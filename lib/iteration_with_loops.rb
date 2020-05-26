1760781f902ef82e724987cc63daaa168f6245bf
Lauren Frankovich <frankovister@gmail.com>
2020-05-26 01:52:47 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,7 +1,7 @@
 def find_min_in_nested_arrays(src)
-  outer_results = []
-  row_index = 0
-    while row_index < src.count do
+outer_results = []
+row_index = 0
+while row_index < src.count do
       element_index = 0
       longest_string_element = ""
         while element_index < src[row_index].count do
